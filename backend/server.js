9dea36932807e6af4814af7ceb81df96dd82b844
fcortescerna <franciscocortes.fc78@gmail.com>
2025-04-28 19:46:46 -0600

    primer commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const connectDB = require("./config/db")
+const port = process.env.PORT || 5000
+const {errorHandler} = require("./middleware/errorMiddleWare")
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/tareas', require ('./routes/tareasRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, ()=> console.log(`Servidor iniciando en el puerto ${port}`))
