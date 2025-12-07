c005412fc5c1dc47e01811b310060a6dc4a710ea
fcortescerna <franciscocortes.fc78@gmail.com>
2025-12-06 21:54:41 -0600

    Cambios para infra

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use('/api/productos', require ('./routes/productosRoutes.js'))
+app.use('/api/productos', require('./routes/productosRoutes.js'))
+
+app.get("/", (req, res) => {
+  res.send("Todo funcionando correctamente");
+});
 
 app.use(errorHandler)
 
